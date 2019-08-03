3f683d416097e3cf895c20fcc2d33cd1f6188cec
lwq <610744313@qq.com>
2019-08-03 21:30:22 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export default ({ dispatch, getState }: { dispatch: any; getState: any }) => (next: any) => (
+  action: any,
+  params: any
+) => {
+  if (typeof action == 'string') {
+    action = {
+      type: action,
+      params
+    }
+  }
+  return next(action)
+}
